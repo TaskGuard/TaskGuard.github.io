842a81ea383d9d253fde27ae28c08836827e6db3
o-su <xmbvztcxzc@email.cz>
2023-11-06 22:22:37 +0100

    added blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 
 import { Layout } from "./common/components/layout";
+import { HowToDistributeYourApps } from "./domains/blog/articles/howtoDistributeYourApps";
+import { BlogPage } from "./domains/blog/blogPage";
 import { DownloadPage } from "./domains/download/donwloadPage";
 import { NotFoundPage } from "./domains/errorPages/notFoundPage";
 import { FeaturesPage } from "./domains/features/featuresPage";
@@ -13,6 +15,8 @@ function App() {
         <Route index element={<Homepage />} />
         <Route path="download" element={<DownloadPage />} />
         <Route path="features" element={<FeaturesPage />} />
+        <Route path="blog" element={<BlogPage />} />
+        <Route path="blog/how-to-distribute-your-apps" element={<HowToDistributeYourApps />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
